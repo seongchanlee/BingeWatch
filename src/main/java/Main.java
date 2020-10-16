bfdcfd9f51f1a949e8f9253e31e594a800a71c6d
seongchanlee <seongchan.lee96@gmail.com>
2020-10-15 18:23:57 -0700

    Added JAR building plugin, build options, and faulty argument handling

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,11 @@ import javax.security.auth.login.LoginException;
 
 public class Main {
     public static void main(String[] args) throws LoginException {
+        if (args.length < 1) {
+            System.out.println("Usage: java -jar yourjarfile.jar [token]");
+            return;
+        }
+
         String credential = args[0];
 
         JDABuilder builder = JDABuilder.createDefault(credential);
